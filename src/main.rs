24702124ec697a36fb386005748c12ec34fdbace
Harshad Hegde <hegdehb@gmail.com>
2023-02-06 15:24:54 -0600

    implemented closures

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::collections::{HashSet, HashMap};
-use csv::ReaderBuilder;
+use std::{collections::{HashSet, HashMap}, fs::File};
+use csv::{ReaderBuilder, Reader};
 
 fn main() {
     // let set1: HashSet<&str> = ["apple", "banana", "cherry"].iter().cloned().collect();
@@ -10,23 +10,17 @@ fn main() {
     csv::ReaderBuilder instead of just csv::Reader because we need to specify
     that the file has no headers.
     */
-    let mut reader = ReaderBuilder::new().has_headers(false)
-                                         .from_path("test_set.tsv")
-                                         .unwrap();
-    let mut dict_from_csv: HashMap<String, HashSet<String>> = HashMap::new();
+    // let closure_reader = read_file("closures.tsv");
 
-    for result in reader.records() {
-        let record = result.unwrap();
-        let name = &record[0];
-        let food = &record[1];
-        let n = dict_from_csv.entry(name.to_string());
-        n.or_default().insert(food.to_string());
-    }
+    let data_dict = dict_from_csv(read_file("test_set.tsv"));
+    let closures_dict = dict_from_csv(read_file("closures.tsv"));
 
     // iterate over dict
-    for (name, foods) in &dict_from_csv {
-        println!("CSV read as key : value => {name} : {foods:?}");
-        let score:f64 = jaccard_similarity(dict_from_csv.get("set1").unwrap(), &foods);
+    for (name, foods) in &data_dict {
+        println!("Original HashMap : key => {name} ; value: {foods:?}");
+        let expanded_foods = expand(foods, &closures_dict);
+        println!("Expanded HashMap : key => {name} ; value: {expanded_foods:?}");
+        let score:f64 = jaccard_similarity(data_dict.get("set1").unwrap(), &expanded_foods);
         println!("score : {score:?}")
     }
 }
@@ -37,4 +31,30 @@ fn jaccard_similarity(set1: &HashSet<String>, set2: &HashSet<String>) -> f64 {
     intersection as f64 / union_measure as f64
 }
 
+fn read_file(filename: &str) -> Reader<File> {
+    ReaderBuilder::new().has_headers(false)
+                        .from_path(filename)
+                        .unwrap()
+}
 
+fn dict_from_csv(mut reader: Reader<File>) -> HashMap<String, HashSet<String>> {
+    
+    let mut dict_from_csv: HashMap<String, HashSet<String>> = HashMap::new();
+
+    for result in reader.records() {
+        let record = result.unwrap();
+        let key = &record[0];
+        let value = &record[1];
+        let n = dict_from_csv.entry(key.to_string());
+        n.or_default().insert(value.to_string());
+    }
+    dict_from_csv
+}
+
+fn expand(set:&HashSet<String> , map: &HashMap<String, HashSet<String>>) -> HashSet<String> {
+    let mut expanded_set = HashSet::<String>::new();
+    for item in set.iter() {
+        expanded_set.extend(map.get(item).unwrap().clone());
+    }
+    expanded_set
+}
