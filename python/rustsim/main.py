bc4ad14f6913c4c9c267642f1155b14da764c7e0
Harshad Hegde <hegdehb@gmail.com>
2023-02-10 07:45:56 -0600

    if name =main

--- python/rustsim/main.py
+++ python/rustsim/main.py
@@ -2,7 +2,11 @@
 import rustsim
 from pathlib import Path
 
-proj_dir = Path(__file__).resolve().parents[3]
-test_data_dir = proj_dir/"tests/data/"
-# This doesn't work for now.
-rustsim.run(test_data_dir/"test_set.tsv", test_data_dir/"closures.tsv")
+def main():
+    proj_dir = Path(__file__).resolve().parents[3]
+    test_data_dir = proj_dir/"tests/data/"
+    # This doesn't work for now.
+    rustsim.run(test_data_dir/"test_set.tsv", test_data_dir/"closures.tsv")
+
+if __name__ == "__main__":
+    main()
