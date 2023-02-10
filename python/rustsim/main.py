0b2ea1784065e49866ca7c3ac78025200c4e28cf
Harshad Hegde <hegdehb@gmail.com>
2023-02-10 07:40:36 -0600

    basic pytohn wrapper code to check rust connection

--- python/rustsim/main.py
+++ python/rustsim/main.py
@@ -1,1 +1,8 @@
 # Python wrapper code for rustsim.
+import rustsim
+from pathlib import Path
+
+proj_dir = Path(__file__).resolve().parents[3]
+test_data_dir = proj_dir/"tests/data/"
+# This doesn't work for now.
+rustsim.run(test_data_dir/"test_set.tsv", test_data_dir/"closures.tsv")
