e73e12832eb229313cee2af4195d69ef2b938bfb
Harshad Hegde <hegdehb@gmail.com>
2023-02-21 15:29:35 -0600

    removed python dir and moved up py code 1 level

--- rustsim/main.py
+++ rustsim/main.py
@@ -6,7 +6,7 @@ import rustsim
 
 def main():
     """Python code for rustsim."""
-    proj_dir = Path(__file__).resolve().parents[2]
+    proj_dir = Path(__file__).resolve().parents[1]
     test_data_dir = proj_dir / "tests/data/"
     rustsim.run(str(test_data_dir / "test_set.tsv"), str(test_data_dir / "closures.tsv"))
 
