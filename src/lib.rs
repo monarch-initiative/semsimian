8b625580fe5982946fe5eaae2565f0d7275d946d
Justin Reese <justaddcoffee@gmail.com>
2023-05-11 20:04:11 -0400

    Remove unused imports, move phenomizer to semsimian

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,12 @@
 use std::{
     collections::{HashMap, HashSet},
-    path::Path,
 };
 
 use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
-mod file_io;
 pub mod utils;
-use file_io::{parse_associations, read_file};
 pub mod similarity;
-use similarity::{
-    calculate_jaccard_similarity, calculate_max_information_content, calculate_phenomizer_score,
-    calculate_semantic_jaccard_similarity, get_most_recent_common_ancestor_with_score,
-};
-pub mod closures;
-use closures::expand_terms_using_closure;
-pub mod structs;
-use structs::TermSetPairwiseSimilarity;
-pub mod ancestors;
-use ancestors::get_intersection_between_sets;
-use utils::{convert_list_of_tuples_to_hashmap, numericize_sets};
+use similarity::{*};
+use utils::{*};
 
 
 #[derive(Default)]
@@ -48,10 +35,12 @@ impl RustSemsimian {
 
     // TODO: implement max IC (what do we call this? max information content?, resnik_similarity?)
 
-    // this is not terribly useful, it's just the IC of a term
-    pub fn information_content(&self, term: &str) -> f64 {
-        let ic = self.ic_map.get(term).unwrap_or(&0.0);
-        *ic
+    pub fn phenomizer_score(
+        map: HashMap<String, HashMap<String, f64>>,
+        entity1: HashSet<String>,
+        entity2: HashSet<String>,
+    ) -> PyResult<f64> {
+        Ok(calculate_phenomizer_score(map, entity1, entity2))
     }
 
     // TODO: deal with predicates (see working code elsewhere in this repo)
@@ -89,62 +78,12 @@ impl Semsimian {
         Ok(self.ss.jaccard_similarity(term1, term2, predicates))
     }
 
-    fn information_content(&self, term: &str) -> PyResult<f64> {
-        Ok(self.ss.information_content(term))
-    }
 }
 
 #[pymodule]
 fn semsimian(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Semsimian>()?;
     Ok(())
-}
-
-
-#[pyfunction]
-fn mrca_and_score(map: HashMap<String, f64>) -> PyResult<(String, f64)> {
-    Ok(get_most_recent_common_ancestor_with_score(map))
-}
-
-
-#[pyfunction]
-fn semantic_jaccard_similarity(
-    closure_table: HashMap<String, HashMap<String, HashSet<String>>>,
-    entity1: String,
-    entity2: String,
-    predicates: Option<HashSet<String>>,
-) -> PyResult<f64> {
-    Ok(calculate_semantic_jaccard_similarity(
-        &closure_table,
-        entity1,
-        entity2,
-        &predicates,
-    ))
-}
-
-#[pyfunction]
-fn max_information_content(
-    closure_table: HashMap<String, HashMap<String, HashSet<String>>>,
-    entity1: String,
-    entity2: String,
-    predicates: Option<HashSet<String>>,
-) -> PyResult<f64> {
-    Ok(calculate_max_information_content(
-        &closure_table,
-        &entity1,
-        &entity2,
-        &predicates,
-    ))
-}
-
-
-#[pyfunction]
-fn phenomizer_score(
-    map: HashMap<String, HashMap<String, f64>>,
-    entity1: HashSet<String>,
-    entity2: HashSet<String>,
-) -> PyResult<f64> {
-    Ok(calculate_phenomizer_score(map, entity1, entity2))
 }
 
 
