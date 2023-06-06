4b49be36add1f357abf088af1cfc3afda1919327
Harshad Hegde <hegdehb@gmail.com>
2023-06-06 14:50:41 -0500

    cleanup

--- src/similarity.rs
+++ src/similarity.rs
@@ -159,20 +159,6 @@ fn common_ancestors(
         .collect()
 }
 
-// scores: maps ancestors to corresponding IC scores
-// fn _mica_and_score(scores: &HashMap<TermID, f64>) -> (Option<TermID>, f64) {
-//     let mut max_ic = 0.0;
-//     let mut mica = None;
-
-//     for (ancestor, ic) in scores.iter() {
-//         if *ic > max_ic {
-//             max_ic = *ic;
-//             mica = Some(ancestor.clone());
-//         }
-//     }
-//     (mica, max_ic)
-// }
-
 #[cfg(test)]
 mod tests {
     use crate::utils::numericize_sets;
