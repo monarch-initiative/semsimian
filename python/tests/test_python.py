a7b473833d6d6be31be42467fcbcf6a87e235772
Harshad Hegde <hegdehb@gmail.com>
2023-08-03 10:03:00 -0500

    converted set to list

--- python/tests/test_python.py
+++ python/tests/test_python.py
@@ -20,21 +20,21 @@ class testSemsimianWithPython(unittest.TestCase):
     def test_jaccard_similarity(self):
         term1 = "apple"
         term2 = "banana"
-        predicates = {"related_to"}
+        predicates = ["related_to"]
         result = self.semsimian.jaccard_similarity(term1, term2, predicates)
         self.assertEqual(result, 1.0 / 3.0)
 
     def test_resnik_similarity(self):
         term1 = "apple"
         term2 = "banana"
-        predicates = {"related_to"}
+        predicates = ["related_to"]
         result = self.semsimian.resnik_similarity(term1, term2, predicates)
         self.assertEqual(result, ({"banana"}, 1.3219280948873622))
 
     def test_all_by_all_pairwise_similarity(self):
         subject_terms = {"apple", "banana", "orange"}
         object_terms = {"orange", "pear", "kiwi"}
-        predicates = {"related_to"}
+        predicates = ["related_to"]
         orange_mica = {"orange", "pear"}
         result = self.semsimian.all_by_all_pairwise_similarity(
             subject_terms, object_terms, 0.0, 0.0, predicates
