b16b80612c5298531e800721a02c6bc55ea4a52b
Harshad Hegde <hegdehb@gmail.com>
2023-08-25 10:08:25 -0500

    change time difference logic

--- python/tests/test_python.py
+++ python/tests/test_python.py
@@ -93,15 +93,15 @@ class testSemsimianWithPython(unittest.TestCase):
             pairwise_similarity_attributes=None,
             resource_path=self.db,
         )
-        load_start = round(time.time(), 10)
+        load_start = time.time()
         _ = semsimian.termset_pairwise_similarity(subject_terms, object_terms)
-        interval_1 = round(round(time.time(), 10) - load_start, 10)
+        interval_1 = time.time() - load_start
         logging.debug(f"Warmup time: {interval_1} sec")
-        second_compare_time = round(time.time(), 10)
+        second_compare_time = time.time()
         _ = semsimian.termset_pairwise_similarity(subject_terms, object_terms)
-        interval_2 = round(round(time.time(), 10) - second_compare_time, 10)
+        interval_2 = time.time() - second_compare_time
         logging.debug(f"Second compare time: {interval_2} sec")
-        self.assertGreater(interval_1, interval_2)
+        self.assertTrue(interval_1 - interval_2 >= 0)
 
 
 if __name__ == "__main__":
