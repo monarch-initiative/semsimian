014e30234d5edba67ab44a3108a8141885941ba0
Justin Reese <justaddcoffee@gmail.com>
2023-10-04 14:17:18 -0400

    Added termset_pairwise_similarity_weighted_negated() method

--- src/lib.rs
+++ src/lib.rs
@@ -437,6 +437,99 @@ impl RustSemsimian {
         )
     }
 
+    pub fn termset_pairwise_similarity_weighted_negated(
+        &self,
+        subject_terms: &HashSet<TermID>,
+        object_terms: &HashSet<TermID>,
+        weights: &HashMap<TermID, f64>,
+        negated_terms: &HashSet<TermID>,
+    ) -> TermsetPairwiseSimilarity {
+        /// Compares a set of subject terms to a set of object terms and returns a TermsetPairwiseSimilarity object.
+        /// This function is similar to termset_pairwise_similarity, but it accepts weights for each term,
+        /// such that the similarity score is weighted by the term's weight.
+        ///
+        /// Also, this function accepts a set of negated terms, which are terms that have been excluded
+        /// by the clinician.
+        ///
+        /// This is used for example in N3C work, in which we have a Spark DF of "profiles":
+        /// profile_id  term_id  weight  negated
+        /// 1           HP:1234  0.5     false
+        /// 1           HP:5678  0.2     false
+        /// 1           HP:9012  0.3     true
+        /// 2           HP:6789  0.3     false
+        /// 2           HP:3456  0.7     false
+        /// ...
+        ///
+        /// and a Spark DF of Patient phenotypes:
+        /// patient_id  term_id  negated
+        /// 1           HP:1234  false
+        /// 1           HP:5678  false
+        /// 1           HP:9012  false
+        /// 2           HP:6789  false
+        /// 2           HP:3456  true
+        /// ...
+        ///
+        /// # Arguments
+        ///
+        ///        subject_terms: a HashSet of the terms for termset 1
+        //         object_terms: a HashSet of the terms for termset 2
+        //         weights: a HashMap of the terms and their weights
+        //         negated_terms: a HashSet of the terms that have been ruled out by the clinician
+
+        let metric = "ancestor_information_content_negated_weighted";
+        let all_by_all: SimilarityMap =
+            self.all_by_all_pairwise_similarity(subject_terms, object_terms, &None, &None);
+
+        let mut all_by_all_object_perspective: SimilarityMap =
+            HashMap::with_capacity(all_by_all.len());
+        for (key1, value1) in all_by_all.iter() {
+            for (key2, value2) in value1.iter() {
+                all_by_all_object_perspective
+                    .entry(key2.to_owned())
+                    .or_insert_with(HashMap::new)
+                    .insert(key1.to_owned(), value2.to_owned());
+            }
+        }
+        let db_path = RESOURCE_PATH.lock().unwrap();
+        let all_terms: HashSet<String> = subject_terms
+            .iter()
+            .chain(object_terms.iter())
+            .cloned()
+            .collect();
+        let all_terms_vec: Vec<String> = all_terms.into_iter().collect();
+        let term_label_map = get_labels(db_path.clone().unwrap().as_str(), &all_terms_vec).unwrap();
+
+        let subject_termset: Vec<BTreeMap<String, BTreeMap<String, String>>> =
+            get_termset_vector(subject_terms, &term_label_map);
+        let object_termset: Vec<BTreeMap<String, BTreeMap<String, String>>> =
+            get_termset_vector(object_terms, &term_label_map);
+        let average_termset_information_content = &self
+            .termset_comparison(subject_terms, object_terms)
+            .unwrap();
+
+        let (subject_best_matches, subject_best_matches_similarity_map) =
+            get_best_matches(&subject_termset, &all_by_all, &term_label_map, metric);
+        let (object_best_matches, object_best_matches_similarity_map) = get_best_matches(
+            &object_termset,
+            &all_by_all_object_perspective,
+            &term_label_map,
+            metric,
+        );
+        let best_score = get_best_score(&subject_best_matches, &object_best_matches);
+
+        TermsetPairwiseSimilarity::new(
+            subject_termset,
+            subject_best_matches,
+            subject_best_matches_similarity_map,
+            object_termset,
+            object_best_matches,
+            object_best_matches_similarity_map,
+            *average_termset_information_content,
+            best_score,
+            metric.to_string(),
+        )
+    }
+
     // This function takes a set of objects and an expanded subject map as input.
     // It expands each object using closure and calculates the Jaccard similarity score between each subject and object.
     // The result is a vector of tuples containing the score, an optional TermsetPairwiseSimilarity, and the TermID.
@@ -1240,6 +1333,22 @@ mod tests {
         let mut rss = RustSemsimian::new(None, predicates, None, db);
         rss.update_closure_and_ic_map();
         let tsps = rss.termset_pairwise_similarity(&subject_terms, &object_terms);
+        assert_eq!(tsps.average_score, 5.4154243283740175);
+        assert_eq!(tsps.best_score, 5.8496657269155685);
+    }
+    #[test]
+    fn test_termset_pairwise_similarity_weighted_negated() {
+        let db = Some("tests/data/go-nucleus.db");
+        // Call the function with the test parameters
+        let predicates: Option<Vec<Predicate>> = Some(vec![
+            "rdfs:subClassOf".to_string(),
+            "BFO:0000050".to_string(),
+        ]);
+        let subject_terms = HashSet::from(["GO:0005634".to_string(), "GO:0016020".to_string()]);
+        let object_terms = HashSet::from(["GO:0031965".to_string(), "GO:0005773".to_string()]);
+        let mut rss = RustSemsimian::new(None, predicates, None, db);
+        rss.update_closure_and_ic_map();
+        let tsps = rss.termset_pairwise_similarity_weighted_negated(&subject_terms, &object_terms);
         assert_eq!(tsps.average_score, 5.4154243283740175);
         assert_eq!(tsps.best_score, 5.8496657269155685);
     }
